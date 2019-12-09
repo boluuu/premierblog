679c93c37e4ec99363ab1267dcd9a8f167e063ab
Boluuu <odedeyib@gmail.com>
2019-12-09 21:05:21 +0100

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+STATUS = (
+    (0, "Drafts"),
+    (1, "Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    image = models.ImageField(upload_to='post_image', blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Meta:
+    ordering = ['-created_on']
+
+def __str__(self):
+    return self.title
